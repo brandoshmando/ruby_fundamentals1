5ebd57a9f68de5ba6996a402f89fee210342b5cf
brandoshmando <brancraft@gmail.com>
2014-06-03 12:23:44 -0400

    adding excersize2.rb

---
+++ excersize2.rb
@@ -0,0 +1,19 @@
+#Calcualate a valid tip (United States 20%)
+tip = 55
+
+puts tip * 1.2
+
+#Adding a string and an integer
+
+puts "What a quaint little string plus the integer " + 25.to_s + " in string form."
+
+#Embedded substitution
+
+a = 45628 
+b = 7839
+puts "#{a+b}"
+
+#result of (true && false) || (true && false) || !(true && false)
+
+puts true
+
